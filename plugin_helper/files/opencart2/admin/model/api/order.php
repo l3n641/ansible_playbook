86fde69486bcfd223da5dc7b14bc6d2f0b044f7e
l3n641 <371522155@qq.com>
2021-04-27 18:44:21 +0800

    update  plugin_helper 订单添加产品详情

--- plugin_helper/files/opencart2/admin/model/api/order.php
+++ plugin_helper/files/opencart2/admin/model/api/order.php
@@ -6,8 +6,34 @@ class ModelApiOrder extends Model
    public function getProcessingOrder(){
         $sql = sprintf("select *  from global_data.`orders`  where order_status_id=%d ",1);
         $result = $this->db->query($sql);
+        $ordes=[];
+        if ( $result->rows){
+           foreach($result->rows as $order){
+           $sql = sprintf("select *  from global_data.`order_products`  where order_id=%d ", $order["id"]);
+           $products_query = $this->db->query($sql);
+           $products=[];
+           foreach($products_query->rows as $product) {
 
-        return $result->rows;
+            $products[]=[
+                "product_id"=> $product["product_id"],
+                "name"=> $product["name"],
+                "model"=> $product["model"],
+                "quantity"=> $product["quantity"],
+                "color"=> $product["color"],
+                "size"=> $product["size"],
+                "price"=> $product["price"],
+                "total"=> $product["total"],
+                "tax"=> $product["tax"],
+                "reward"=> $product["reward"],
+            ];
+
+           }
+            $order["products"]=$products;
+            $ordes[]=$order;
+           }
+
+        }
+        return $ordes;
     }
 
 }
